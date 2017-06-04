d442a2fa8d7c38419eab72dc51a52a0e78df4c04
kid93 <uikid93@gmail.com>
2017-06-04 12:17:29 +0100

    layout improvement

--- ExtractBrandFromUrl/Export.cs
+++ ExtractBrandFromUrl/Export.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace ExtractBrandFromUrl
 {
-	class Export
-	{
+    class Export
+    {
 
         private List<string> _finalUrls = new List<string>();
 
 
         public void ToCSV(List<string> original, List<string> cleansed, List<string> comparisonResult)
-		{
+        {
             for (var i = 0; i < original.Count; i++)
-			{
-				_finalUrls.Add(original[i] + "," + cleansed[i] + "," + comparisonResult[i]);
-			}
+            {
+                _finalUrls.Add(original[i] + "," + cleansed[i] + "," + comparisonResult[i]);
+            }
 
             File.WriteAllLines("new.csv", _finalUrls, Encoding.Default);
-		}
+        }
 
-	}
+    }
 }
